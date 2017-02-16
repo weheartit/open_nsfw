922f27909c48d10c34fe49ddf4301ba16a315f98
David O'Dell <davidodell@gmail.com>
2017-02-16 11:31:19 -0800

    added script to test one image via url

---
+++ fetch_by_url.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget $1 -O test.jpg
+sudo docker run --volume=$(pwd):/workspace 243284212115.dkr.ecr.us-east-1.amazonaws.com/whi_nsfw:15FEB2017 \
+python ./classify_nsfw.py \
+--model_def nsfw_model/deploy.prototxt \
+--pretrained_model nsfw_model/resnet_50_1by2_nsfw.caffemodel \
+test.jpg
+rm test.jpg
